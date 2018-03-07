f57a78dbb09b0ae95d86ef5cd9d9d019639945d9
Jim Brisco <jgbrisco@gmail.com>
2018-03-07 12:38:24 -0800

    Adding code from https://www.codeproject.com/Articles/18734/Method-Overriding-in-C

--- TestInheritenceGithub/Program.cs
+++ TestInheritenceGithub/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace TestInheritenceGithub
+namespace TestInheritence
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            BC b;
+            b = new BC();
+            b.Display();
+
+            b = new DC();
+            b.Display();
+
+            b = new TC();
+            b.Display();
+
+            TC c;
+            c = new TC();
+            c.Display();
+
+            Console.ReadLine();
+        }
+    }
+    class BC  // Base Class
+    {
+        public virtual void Display()
+        {
+            System.Console.WriteLine("BC::Display");
+        }
+    }
+
+    class DC : BC   // Derived Class
+    {
+        public override void Display()
+        {
+            System.Console.WriteLine("DC::Display");
+        }
+    }
+    class TC : BC
+    {
+        new public void Display()
+        {
+            System.Console.WriteLine("TC::Display");
         }
     }
 }
