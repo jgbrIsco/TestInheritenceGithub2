a1ce74c128862732fbb65d685f88910a98fa4287
Jim Brisco <jgbrisco@gmail.com>
2018-03-07 12:24:56 -0800

    Add project files.

---
+++ TestInheritenceGithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestInheritenceGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
