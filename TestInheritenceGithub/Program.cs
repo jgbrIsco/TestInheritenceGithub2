10adb55defe2afc0474f6047e029eeb2fd2705ac
Jim Brisco <jgbrisco@gmail.com>
2018-03-07 12:31:40 -0800

    First commit

--- TestInheritenceGithub/Program.cs
+++ TestInheritenceGithub/Program.cs
@@ -6,7 +6,7 @@ namespace TestInheritenceGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
